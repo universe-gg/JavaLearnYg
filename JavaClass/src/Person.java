425723670d3484a5a85e5437dda629770c813529
yguo <11071752@qq.com>
2020-05-02 20:23:57 +0800

    JAVA知识点测试。

--- JavaClass/src/Person.java
+++ JavaClass/src/Person.java
@@ -5,16 +5,19 @@
 public class Person {
     private String name;
     private String birthday;
+    private static String nationality = "中国";
+    private static int instanceCout = 0;
 
     public Person() {
-        System.out.println("Person.Person()");
-        System.out.println("name = " + name);
-        System.out.println("birthday = " + birthday);
+        System.out.println("...Person.Person()");
+        instanceCout++;
+        System.out.println("name = " + this.name);
+        System.out.println("birthday = " + this.birthday);
     }
 
     public Person(String name, String birthday) {
-        System.out.println("Person.Person(" + name + ", " + birthday + ")");
-
+        System.out.println("...Person.Person(" + name + ", " + birthday + ")");
+        instanceCout++;
         this.name = name;
         this.birthday = birthday;
 
@@ -23,37 +26,62 @@ public class Person {
     }
 
     public String getBirthday() {
-        System.out.println("Person.getBirthday");
-        return birthday;
+        System.out.println("...Person.getBirthday()");
+        return this.birthday;
     }
 
     public void setBirthday(String birthday) {
-        System.out.println("Person.setBirthday");
+        System.out.println("...Person.setBirthday(" + birthday + ")");
         this.birthday = birthday;
     }
 
     public String getName() {
-        System.out.println("Person.getName");
-        return name;
+        System.out.println("...Person.getName()");
+        return this.name;
     }
 
     public void setName(String name) {
-        System.out.println("Person.setName");
+        System.out.println("...Person.setName(" + name + ")");
         this.name = name;
+    }
+
+    public static String getNationality() {
+        System.out.println("...Person.getNationality()");
+        return Person.nationality;
+    }
+
+    public static void setNationality(String nationality) {
+        System.out.println("...Person.setNationality()");
+        Person.nationality = nationality;
+    }
+
+    public static int getInstanceCount() {
+        System.out.println("...Person.getInstanceCount()");
+        return Person.instanceCout;
+    }
+
+    public static void setInstanceCout(int instanceCout) {
+        System.out.println("...Person.setInstanceCout()");
+        Person.instanceCout = instanceCout;
     }
 
     @Override
     public String toString() {
-        System.out.println("Person.toString");
+        System.out.println("...Person.toString()");
         StringBuffer strb = new StringBuffer("name = " + this.name + "\n");
         strb.append("birthday = " + this.birthday + "\n");
+        strb.append("nationality = " + Person.nationality + "\n");
+        strb.append("instanceCount = " + Person.instanceCout + "\n");
         String s = super.toString() + "\n" +  strb.toString();
         return s;
     }
 
     public void println() {
-        System.out.println("name = " + name);
-        System.out.println("birthday = " + birthday);
+        System.out.println("...Person.println()");
+        System.out.println("name = " + this.name);
+        System.out.println("birthday = " + this.birthday);
+        System.out.println("nationality = " + Person.nationality);
+        System.out.println("instanceCout = " + Person.instanceCout);
     }
 
 }
